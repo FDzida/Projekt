a01dfbb466144bf885b663e76930bf30a4ba18dd
FDzida <filipdzida2@gmail.com>
2026-08-01 13:49:56 +0200

    Zkouzka kodu v ukolu

--- Main.py
+++ Main.py
@@ -1,2 +1,17 @@
-print("Test Github public")
-print("Test n.2")
+samohlasky = 'aeiouáéíóú'
+souhlasky = 'bcčdďfghjklmnňprřsštťvzž'
+vysledky = {'souhlasky': 0, 'samohlasky': 0}
+veta = 'Zvuk řeči je produkován otevřenou konfigurací vokálního traktu'
+
+for znak in veta:
+    if not znak.isalpha():
+        continue
+
+    znak_lower = znak.lower()
+
+    if znak_lower in samohlasky:
+        vysledky["samohlasky"] += 1
+    else:
+        vysledky["souhlasky"] += 1
+
+print(vysledky)
